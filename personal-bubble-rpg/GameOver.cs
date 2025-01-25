e386b26f21e355ed1f6772715f081c22c31c5857
Kossuranta <anssi.koskenranta@gmail.com>
2025-01-25 23:08:21 +0200

    Game over state

---
+++ personal-bubble-rpg/GameOver.cs
@@ -0,0 +1,90 @@
+using Godot;
+using System;
+
+public partial class GameOver : MarginContainer
+{
+    [Export]
+    private float m_fadeDuration = 1f;
+
+    [Export]
+    private Label m_fadeOutCounter;
+
+    [Export]
+    private float m_delay = 5f;
+
+    private bool m_targetState;
+    private float m_current;
+    private bool m_fading;
+    private float m_delayCounter;
+    private bool m_delayRunning;
+
+    public Action FadeOutCompleted;
+    
+    public override void _Ready()
+    {
+        Color color = Modulate;
+        color.A = 0;
+        Modulate = color;
+    }
+
+    public void Toggle(bool _state, int _fadeOutCount)
+    {
+        GD.Print(_state);
+        m_fading = true;
+        m_targetState = _state;
+        if (_state)
+            m_fadeOutCounter.Text = _fadeOutCount.ToString();
+
+        m_current = _state ? 0f : 1f;
+        Color color = Modulate;
+        color.A = m_current;
+        Modulate = color;
+    }
+    
+    public override void _Process(double _delta)
+    {
+        float delta = (float) _delta;
+        if (m_delayRunning)
+        {
+            m_delayCounter -= delta;
+
+            if (m_delayCounter < 0)
+            {
+                m_delayRunning = false;
+                GameManager.Instance.RestartGame();
+            }
+        }
+        else if (m_fading)
+        {
+            if (m_targetState)
+            {
+                m_current += m_fadeDuration * delta;
+
+                if (m_current >= 1f)
+                {
+                    m_fading = false;
+                    GD.Print("FadeInCompleted");
+                    m_delayRunning = true;
+                    m_delayCounter = m_delay;
+                }
+            }
+            else
+            {
+                m_current -= m_fadeDuration * delta;
+
+                if (m_current <= 0f)
+                {
+                    m_fading = false;
+                    GD.Print("FadeOutCompleted");
+                    FadeOutCompleted?.Invoke();
+                }
+            }
+
+            m_current = Mathf.Clamp(m_current, 0, 1f);
+            GD.Print(m_current);
+            Color color = Modulate;
+            color.A = m_current;
+            Modulate = color;
+        }
+    }
+}
--- personal-bubble-rpg/Managers/GameManager.cs
+++ personal-bubble-rpg/Managers/GameManager.cs
@@ -16,43 +16,68 @@ public partial class GameManager : Node
 	[Export]
 	private PackedScene m_fightScenePrefab;
 
+	[Export]
+	private PackedScene m_gameOverPrefab;
+
 	public static Node3D MainLevel { get; private set; }
 	public static Camera3D Camera { get; private set; }
 
 	private Hud m_hud;
 	private FightEncounter m_fightScene;
+	private GameOver m_gameOver;
 	
 	public int RunCounter { get; private set; }
+	
+	public bool GameRunning { get; private set; }
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		Instance = this;
-		StartGame();
+		StartGame(true);
 	}
 
 	public void GameOver()
 	{
-		StartGame();
+		GameRunning = false;
+		m_gameOver.Toggle(true, RunCounter);
 	}
 
-	public void StartGame()
+	public void RestartGame()
 	{
+		StartGame(false);
+	}
+	
+	private void StartGame(bool _instant)
+	{
+		GD.Print("StartGame");
 		RunCounter++;
 		MainLevel?.QueueFree();
 		m_hud?.QueueFree();
 		Camera?.QueueFree();
 		m_fightScene?.QueueFree();
+		m_gameOver?.QueueFree();
 		
 		InitLevel();
 		InitHud();
 		InitCamera();
+		InitGameOver();
+
+		if (_instant)
+		{
+			FadeOutCompleted();
+		}
+		else
+		{
+			m_gameOver.Toggle(false, RunCounter);
+			m_gameOver.FadeOutCompleted += FadeOutCompleted;
+		}
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double _delta)
+	private void FadeOutCompleted()
 	{
-		
+		GD.Print("Continue");
+		GameRunning = true;
 	}
 
 	public void StartFight(CharacterType _enemy)
@@ -79,5 +104,11 @@ public partial class GameManager : Node
 	{
 		Camera = (Camera3D)m_cameraPrefab.Instantiate();
 		AddChild(Camera);
+	}
+
+	private void InitGameOver()
+	{
+		m_gameOver = (GameOver)m_gameOverPrefab.Instantiate();
+		AddChild(m_gameOver);
 	}
 }
