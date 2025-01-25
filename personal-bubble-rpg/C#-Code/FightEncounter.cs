b671dfaa7983f888d5371fe5266c002d96fc8fd0
Ilma95 <ilmari.huhtanen@gmail.com>
2025-01-25 20:03:13 +0200

    FightScene edited

--- personal-bubble-rpg/C#-Code/FightEncounter.cs
+++ personal-bubble-rpg/C#-Code/FightEncounter.cs
@@ -19,30 +19,39 @@ public partial class FightEncounter : Node
 			switch (characterType)
 			{
 				case CharacterType.ROSVO:
+					// spawns a robber
 					GD.Print("Rosvo");
 					break;
 				case CharacterType.MUMMO:
+					// spawns a granny
 					GD.Print("Mummo");
 					break;
 				case CharacterType.EX:
+					// spawns an ex
 					GD.Print("Ex");
 					break;
 				case CharacterType.FEISSARI:
+					// spawns a yuppie
 					GD.Print("Feissari");
 					break;
 				case CharacterType.TUTTU:
+					// spawns a friend
 					GD.Print("Tuttu");
 					break;
 				case CharacterType.TURISTI:
+					// spawns a tourist
 					GD.Print("Turisti");
 					break;
 				case CharacterType.TERAPEUTTI:
+					// spawns a therapist
 					GD.Print("Terapeutti");
 					break;
 				case CharacterType.KOIRA:
+					// spawns a dog
 					GD.Print("Koira");
 					break;
 				case CharacterType.OVI:
+					// spawns a door
 					GD.Print("Ovi");
 					break;
 				default:
