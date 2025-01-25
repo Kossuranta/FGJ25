fec0393140ac21c7c8d782f9e0e6819aa1a9e52e
Kossuranta <anssi.koskenranta@gmail.com>
2025-01-25 12:50:34 +0200

    Rotate player

---
+++ personal-bubble-rpg/GenericScripts/RotateTowardsMovementDir.cs
@@ -0,0 +1,21 @@
+using Godot;
+
+public partial class RotateTowardsMovementDir : Node3D
+{
+	[Export]
+	private CharacterBody3D m_characterBody;
+
+	[Export]
+	private float m_rotationSpeed = 5f;
+
+	public override void _Process(double _delta)
+	{
+		float delta = (float) _delta;
+		Vector3 velocity = m_characterBody.Velocity;
+		if (velocity.Length() != 0)
+		{
+			Vector3 dir = m_characterBody.GlobalPosition + velocity;
+			LookAt(dir);
+		}
+	}
+}
